34217c24371ca71a80af3e83a4efaae83d2bc5e6
Chris Pedro <chris@thepedros.com>
2021-06-22 21:04:27 +0100

    Initial version - added blink.py

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pi Playground
+
+This is just a place for me to play around with Raspberry Pi related things.
+
---
+++ blink.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+"""
+File: blink.py
+Description: Blinks an LED connected to GPIO pin 17 (WiringPi 0)
+"""
+
+
+__author__ = 'Chris Pedro'
+__copyright__ = '(c) Chris Pedro 2021'
+__licence__ = 'MIT'
+
+
+import sys
+import time
+import wiringpi
+
+from signal import signal, SIGINT
+
+
+def main(args):
+    wiringpi.wiringPiSetup()
+     
+    wiringpi.pinMode(0,1)
+     
+    try:
+        while True:
+            loop()
+    except KeyboardInterrupt:
+        destroy()
+
+
+def loop():
+    time.sleep(0.5)
+    wiringpi.digitalWrite(0,1)
+    time.sleep(0.5)
+    wiringpi.digitalWrite(0,0)
+
+
+def destroy():
+    wiringpi.digitalWrite(0,0)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
+
